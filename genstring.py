22e2381017a2b7fb75fe1e31e98c1fee22453b26
HEIMAN PICTURES <78695802+HeimanPictures@users.noreply.github.com>
2021-05-29 08:59:25 +0530

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -1,5 +1,26 @@
-import pyrogram
-from pyrogram import Client
+# Copyright (c) 2021 HEIMAN PICTURES
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
 
 heiman_ = """
 
@@ -14,8 +35,6 @@ print(heiman_)
 api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
-with Client("Heiman", api_id=api_id, api_hash=api_hash) as bot_:
-    first_name = (bot_.get_me()).first_name
-    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @HeimanSupports \n<code>{bot_.export_session_string()}</code>"
-    bot_.send_message("me", string_session_, parse_mode="html")
-    print(f"String Has Been Sent To Your Saved Message : {first_name}")
+with TelegramClient(StringSession(), api_id, api_hash) as client:
+    print(client.session.save())
+    client.send_message("me", client.session.save())

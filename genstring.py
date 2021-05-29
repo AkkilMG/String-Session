ed1808e94e542d2aead3fbb07936d9bf5a540ae1
HEIMAN PICTURES <78695802+HeimanPictures@users.noreply.github.com>
2021-05-29 08:25:07 +0530

    Special
    Added My Channel

--- genstring.py
+++ genstring.py
@@ -16,6 +16,6 @@ api_hash = input("Enter Your API HASH : \n")
 
 with Client("Heiman", api_id=api_id, api_hash=api_hash) as bot_:
     first_name = (bot_.get_me()).first_name
-    string_session_ = f"<b>String Session For {first_name}</b> \n<code>{bot_.export_session_string()}</code>"
+    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @HeimanSupports \n<code>{bot_.export_session_string()}</code>"
     bot_.send_message("me", string_session_, parse_mode="html")
     print(f"String Has Been Sent To Your Saved Message : {first_name}")

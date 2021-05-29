2cf92bb9eb33de4c34f5d824ecb3ad42b49c8aed
HEIMAN PICTURES <78695802+HeimanPictures@users.noreply.github.com>
2021-05-29 10:07:12 +0530

    Update genstring_tele.py

--- genstring_tele.py
+++ genstring_tele.py
@@ -35,5 +35,12 @@ api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print(client.session.save())
-    client.send_message("me", client.session.save())
+    print("Check your Telegram Saved Messages to copy the STRING_SESSION value")
+    session_string = client.session.save()
+    saved_messages_template = """Support: @userbotindo
+<code>STRING_SESSION</code>: <code>{}</code>
+⚠️ <i>Please be carefull to pass this value to third parties</i>""".format(session_string)
+
+
+#   print(client.session.save())
+#    client.send_message("me", client.session.save())
